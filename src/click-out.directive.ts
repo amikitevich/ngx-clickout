e81eba9afd72826a2cc756342619bbcaee77929a
Alexey Mikitevich <alexeymikitevich@pro.local>
2017-06-29 17:46:34 +0300

    remove rx, add angular event listeners. breaking change remove clickOutFilter input property

--- src/click-out.directive.ts
+++ src/click-out.directive.ts
@@ -2,27 +2,10 @@ import {
   Directive,
   Input,
   Output,
-  OnChanges,
   OnInit,
-  OnDestroy,
   ElementRef,
-  SimpleChanges,
-  EventEmitter
+  EventEmitter, HostListener, HostBinding
 } from '@angular/core';
-
-import { Subscription } from 'rxjs/Subscription';
-import { BehaviorSubject } from 'rxjs/BehaviorSubject';
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/observable/fromEvent';
-import 'rxjs/add/observable/merge';
-import 'rxjs/add/observable/timer';
-import 'rxjs/add/operator/filter';
-import 'rxjs/add/operator/skipUntil';
-import 'rxjs/add/operator/takeUntil';
-import 'rxjs/add/operator/mergeMap';
-import 'rxjs/add/operator/do';
-
-const ESC_CODE = 27;
 
 const getEventPath = (event: Event): HTMLElement[] => {
   if (event['path']) {
@@ -39,123 +22,43 @@ const getEventPath = (event: Event): HTMLElement[] => {
   return path;
 };
 
-interface MouseEventExtended extends MouseEvent {
-  path: Node[];
-}
-
 @Directive({
   selector: '[clickOut]'
 })
-export class ClickOutDirective implements OnChanges, OnInit, OnDestroy {
+export class ClickOutDirective implements OnInit {
 
   @Input() clickOut: boolean;
-  @Input() clickOutFilter: string;
 
-  @Output() clickOutEvent: EventEmitter<boolean> = new EventEmitter<boolean>();
+  @Output() clickOutEvent: EventEmitter<any> = new EventEmitter<any>();
 
-  private documentClick$: BehaviorSubject<boolean> = new BehaviorSubject(false);
+  @HostBinding('attr.tabindex') tabindex = -1;
 
-  private clickSub: Subscription;
+  @HostListener('document:mousedown', ['$event']) onMouseDown(event: MouseEvent) {
+
+    if (this.clickOut && !getEventPath(event).includes(this._element.nativeElement)) {
+      this.clickOutEvent.emit();
+    }
+  }
+
+  @HostListener('keydown.esc', ['$event'] ) onEsc(event: KeyboardEvent) {
+    if (!this.clickOut) {
+      return false;
+    }
+
+    if (this._element.nativeElement.contains(event.target)) {
+      this.clickOutEvent.emit();
+    } else {
+      this.setFocus();
+    }
+  }
 
   constructor(private _element: ElementRef) { }
 
-  ngOnChanges(changes: SimpleChanges) {
-    if (changes.hasOwnProperty('clickOut') && changes[ 'clickOut' ] !== null) {
-      this.documentClick$.next(this.clickOut);
-    }
-  }
-
   ngOnInit(): void {
-    this.setTabIndex();
-    this.clickSub = this.getActiveState()
-      .do(state => this.setFocus())
-      .flatMap(
-        (state: boolean) => Observable.merge(
-          this.getClickEvent(),
-          this.getEscEvent()
-        )
-      )
-      .subscribe(
-        () => {
-          this.clickOutEvent.emit(false);
-          console.log('click out');
-        },
-        error => console.warn(error)
-      );
-  }
-  ngOnDestroy(): any {
-    this.clickSub.unsubscribe();
-  }
-
-  getClickEvent(): Observable<any> {
-    return Observable.fromEvent(document, 'click')
-      .skipUntil(Observable.timer(50))
-      .takeUntil(this.getInActiveState())
-      .filter(this.getFilterByFunction().bind(this));
-  }
-
-  getEscEvent(): Observable<any> {
-
-    return Observable.fromEvent(this._element.nativeElement, 'keydown')
-      .filter(this.filterAndSetFocusToParent.bind(this))
-      .do((keyEvent: KeyboardEvent) => keyEvent.preventDefault())
-      .takeUntil(this.getInActiveState());
-  }
-
-  private filterAndSetFocusToParent(keyEvent: KeyboardEvent): boolean {
-    let isInside: boolean = this._element.nativeElement.contains(
-      keyEvent.target
-    );
-
-    if (!isInside) {
-      this.setFocus();
-    }
-
-    return keyEvent.keyCode === ESC_CODE && isInside;
-  }
-
-  private setTabIndex() {
-    this._element.nativeElement.setAttribute('tabIndex', '-1');
+    this.setFocus();
   }
 
   private setFocus() {
     this._element.nativeElement.focus();
   }
-
-  private getFilterByFunction(): Function {
-    let resultFunction: Function;
-    switch (this.clickOutFilter) {
-      case 'rect':
-         resultFunction = this.filterEventByRect;
-        break;
-      case 'target':
-        resultFunction = this.filterEventByTarget;
-        break;
-      default:
-        resultFunction = this.filterEventByTarget;
-    }
-    return resultFunction;
-  }
-
-  private filterEventByRect(event: MouseEvent): boolean {
-    let rect = this._element.nativeElement.getBoundingClientRect();
-    let notNull: boolean = event.clientX > 0 && event.clientY > 0;
-    let outsideByX: boolean = event.clientX <= rect.left || event.clientX >= rect.right;
-    let outsideByY: boolean = event.clientY <= rect.top || event.clientY >= rect.bottom;
-    return notNull && (outsideByX || outsideByY);
-  }
-
-  private filterEventByTarget(event: MouseEventExtended): boolean {
-
-    return !getEventPath(event).includes(this._element.nativeElement);
-  }
-
-  private getActiveState() {
-    return this.documentClick$.filter(isActive => isActive === true);
-  }
-
-  private getInActiveState() {
-    return this.documentClick$.filter(isActive => isActive === false);
-  }
-
 }
